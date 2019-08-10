af0b9403690d17490fb42a54aeb7d657ffc5dfc0
Andrew Zandt-Valentine <andrew@andrewzv.com>
2019-08-10 16:16:58 +0100

    Setup base view controller

---
+++ Moneybox-AndrewZandtValentine/Moneybox-AndrewZandtValentine/Helpers/AppColours.swift
@@ -0,0 +1,23 @@
+//
+//  AppColours.swift
+//  Moneybox-AndrewZandtValentine
+//
+//  Created by Andrew Zandt-Valentine on 10/08/2019.
+//  Copyright © 2019 Andrew Zandt-Valentine. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    //Easily create new colour
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    struct AppColours {
+        static let moneyboxBlue = UIColor(red: 106, green: 189, blue: 186)
+    }
+}
