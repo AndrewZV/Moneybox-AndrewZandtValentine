edc536ffd88306f63c1805042c48b577915d9d0e
Andrew Zandt-Valentine <andrew@andrewzv.com>
2019-08-10 16:30:25 +0100

    Added SnapKit CocoaPod

---
+++ Moneybox-AndrewZandtValentine/Pods/Target Support Files/Pods-Moneybox-AndrewZandtValentine/Pods-Moneybox-AndrewZandtValentine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moneybox_AndrewZandtValentine : NSObject
+@end
+@implementation PodsDummy_Pods_Moneybox_AndrewZandtValentine
+@end
---
+++ Moneybox-AndrewZandtValentine/Pods/Target Support Files/Pods-Moneybox-AndrewZandtValentineTests/Pods-Moneybox-AndrewZandtValentineTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moneybox_AndrewZandtValentineTests : NSObject
+@end
+@implementation PodsDummy_Pods_Moneybox_AndrewZandtValentineTests
+@end
